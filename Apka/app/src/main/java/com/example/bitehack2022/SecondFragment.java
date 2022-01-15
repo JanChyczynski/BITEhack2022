b99797120e42a159d773c4de2b8499c3e9a3a367
Adam <adam.p.kulczycki@gmail.com>
2022-01-15 23:15:37 +0100

    photos are being added to the grid now

--- Apka/app/src/main/java/com/example/bitehack2022/SecondFragment.java
+++ Apka/app/src/main/java/com/example/bitehack2022/SecondFragment.java
@@ -1,6 +1,11 @@
 package com.example.bitehack2022;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -8,8 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
@@ -18,7 +25,8 @@ import com.example.bitehack2022.databinding.FragmentSecondBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 public class SecondFragment extends Fragment {
-
+    private static final int CAMERA_PERMISSION_CODE = 1;
+    private static final int CAMERA_REQUEST_CODE = 2;
     private FragmentSecondBinding binding;
 
     @Override
@@ -32,18 +40,43 @@ public class SecondFragment extends Fragment {
 
             Button btn = new Button(getActivity());
             btn.setGravity(Gravity.CENTER_HORIZONTAL);
-            btn.setText("Aga"+Integer.toString(i));
+            btn.setText("Aga" + Integer.toString(i));
             binding.favoritesGrid.addView(btn);
         }
-
 
 
         return binding.getRoot();
 
     }
 
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == CAMERA_PERMISSION_CODE){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setImageBitmap(imageBitmap);
+            binding.favoritesGrid.addView(imageView);
+        }
+    }
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                try {
+                    startActivityForResult(takePictureIntent, CAMERA_PERMISSION_CODE);
+                } catch (ActivityNotFoundException e) {
+                    // display error state to the user
+                }
+            }
+        });
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
