40ec7e28663c7a72e57d85afdfc6e2bd4dd27512
Adam <adam.p.kulczycki@gmail.com>
2022-01-15 21:26:54 +0100

    Button list is now scrollable and they're all Agas

--- Apka/app/src/main/java/com/example/bitehack2022/SecondFragment.java
+++ Apka/app/src/main/java/com/example/bitehack2022/SecondFragment.java
@@ -28,10 +28,11 @@ public class SecondFragment extends Fragment {
     ) {
         binding = FragmentSecondBinding.inflate(inflater, container, false);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 100; i++) {
+
             Button btn = new Button(getActivity());
             btn.setGravity(Gravity.CENTER_HORIZONTAL);
-            btn.setText("blefs"+Integer.toString(i));
+            btn.setText("Aga"+Integer.toString(i));
             binding.favoritesGrid.addView(btn);
         }
 
@@ -50,14 +51,6 @@ public class SecondFragment extends Fragment {
 //                NavHostFragment.findNavController(SecondFragment.this)
 //                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
                 Log.d("TAGGG", "msg");
-            }
-        });
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
