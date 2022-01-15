cabd4e8ab7ae51c9a137cb74acc17d0f998a0b44
jchyczynski <jasiek.ch16@gmail.com>
2022-01-15 20:49:43 +0100

    Storage simple local memory implementation

---
+++ Apka/app/src/main/java/com/example/bitehack2022/Storage.java
@@ -0,0 +1,50 @@
+package com.example.bitehack2022;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Storage {
+
+    private final static String DOMAIN = "https://wilga.pw:9000"; //mocked
+
+    private final ApiProxy apiProxy;
+    private String accessToken;
+
+    private Set<Product> productsCache;
+//    private List<Product> productsAddQueue;
+//    private List<Product> productsRemoveQueue;
+
+    public Storage() {
+        productsCache = new HashSet<>();
+        apiProxy = new ApiProxy(DOMAIN);
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String registerFridge(){
+        return apiProxy.getNewFridgeToken();
+    }
+
+    public List<Product> getProducts(){
+//        apiProxy.getProducts();
+
+        return new ArrayList<>(productsCache);
+    }
+
+    public List<Product> logToFridge(String newAccessToken){
+        accessToken = newAccessToken;
+        return getProducts();
+    }
+
+    public void addProduct(Product product){
+        productsCache.add(product); //mocked
+    }
+
+    public void removeProduct(Product product){
+        productsCache.remove(product); //mocked
+    }
+}
