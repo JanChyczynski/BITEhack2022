f9937de748d473aa6ed7385d11cd41c4b1caab8e
jchyczynski <jasiek.ch16@gmail.com>
2022-01-16 01:33:07 +0100

    Added bitmap field to Product

--- Apka/app/src/main/java/com/example/bitehack2022/Product.java
+++ Apka/app/src/main/java/com/example/bitehack2022/Product.java
@@ -1,9 +1,21 @@
 package com.example.bitehack2022;
 
+import android.graphics.Bitmap;
+
 import java.util.Date;
 
 public class Product {
     private Date expirationDate;
+    private Bitmap bitmap;
+
+    public Product(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Product(Date expirationDate, Bitmap bitmap) {
+        this.expirationDate = expirationDate;
+        this.bitmap = bitmap;
+    }
 
     public Date getExpirationDate() {
         return expirationDate;
@@ -13,7 +25,11 @@ public class Product {
         this.expirationDate = expirationDate;
     }
 
-    public Product(Date expirationDate) {
-        this.expirationDate = expirationDate;
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
     }
 }
